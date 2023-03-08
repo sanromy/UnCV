56878287840eabb1ba8b3a6504a5119ff071d5ce
sanromy <127260227+sanromy@users.noreply.github.com>
2023-03-08 16:08:49 -0300

    Add files via upload

---
+++ sinta.js
@@ -0,0 +1,5 @@
+
+
+document.getElementById('boton').onclick= function () {
+    console.log("Capturamos el evento click");
+    document.getElementById("demo").innerHTML = "Porque con mis recetas te remontarás a lo mejor de tu infancia"}
